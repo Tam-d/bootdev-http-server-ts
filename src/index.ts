aca2e218999c84642c0f32b93118adbad6e1efa4
Tam <5542463+Tam-d@users.noreply.github.com>
2026-03-09 06:01:06 +0000

    Serve static files from src/app

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import express from "express";
+import { Request, Response } from "express";
 
 const app = express();
 const PORT = 8080;
 
-app.use(express.static("."));
+app.use("/app", express.static("./src/app"));
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);

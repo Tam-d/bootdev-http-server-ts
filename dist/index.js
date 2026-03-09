207fb5c51adc47832cfb083e9477cf9f232669a6
Tam <5542463+Tam-d@users.noreply.github.com>
2026-03-09 06:26:17 +0000

    Add middleware to log non ok responses

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,7 @@ import express from "express";
 const app = express();
 const PORT = 8080;
 app.use("/app", express.static("./src/app"));
+app.use(middlewareLogResponses);
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
@@ -9,4 +10,13 @@ app.get("/healthz", handlerReadiness);
 async function handlerReadiness(req, res) {
     res.set("Content-Type", "text/plain; charset=utf-8");
     res.send("OK");
+}
+function middlewareLogResponses(req, res, next) {
+    res.on("finish", () => {
+        console.log(res.statusCode);
+        if (res.statusCode !== 200) {
+            console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}}`);
+        }
+    });
+    next();
 }
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { Request, Response } from "express";
+import { NextFunction } from "express";
 
 const app = express();
 const PORT = 8080;
 
 app.use("/app", express.static("./src/app"));
+app.use(middlewareLogResponses);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
@@ -15,4 +17,17 @@ app.get("/healthz", handlerReadiness);
 async function handlerReadiness(req: Request, res: Response) : Promise<void> {
     res.set("Content-Type", "text/plain; charset=utf-8");
     res.send("OK");
+}
+
+function middlewareLogResponses(req: Request, res: Response, next: NextFunction) : void {
+    res.on("finish", () => {
+
+        console.log(res.statusCode);
+
+        if(res.statusCode !== 200) {
+            console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}}`);
+        } 
+    });
+
+    next();
 }

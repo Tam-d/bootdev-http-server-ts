a64cece5374854c8bbf27c492d4ef25ec7456429
Tam <5542463+Tam-d@users.noreply.github.com>
2026-03-09 22:28:53 +0000

    Add middleware for metrics on /app, Add endpoint to view metrics, Add endpoint to reset metrics

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,26 @@
 import express from "express";
+import { chirpyStateData } from "./config.js";
 const app = express();
 const PORT = 8080;
+app.use("/app", middlewareMetricsInc);
 app.use("/app", express.static("./src/app"));
 app.use(middlewareLogResponses);
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
 app.get("/healthz", handlerReadiness);
+app.get("/metrics", handlerRequestCount);
+app.get("/reset", handlerResetRequestCount);
 async function handlerReadiness(req, res) {
+    res.set("Content-Type", "text/plain; charset=utf-8");
+    res.send("OK");
+}
+async function handlerRequestCount(req, res) {
+    res.set("Content-Type", "text/plain; charset=utf-8");
+    res.send(`Hits: ${chirpyStateData.fileserverHits}`);
+}
+async function handlerResetRequestCount(req, res) {
+    chirpyStateData.fileserverHits = 0;
     res.set("Content-Type", "text/plain; charset=utf-8");
     res.send("OK");
 }
@@ -18,5 +31,9 @@ function middlewareLogResponses(req, res, next) {
             console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}}`);
         }
     });
+    next();
+}
+function middlewareMetricsInc(req, res, next) {
+    chirpyStateData.fileserverHits++;
     next();
 }
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import { Request, Response } from "express";
 import { NextFunction } from "express";
+import { chirpyStateData } from "./config.js";
 
 const app = express();
 const PORT = 8080;
 
+
+app.use("/app", middlewareMetricsInc);
 app.use("/app", express.static("./src/app"));
 app.use(middlewareLogResponses);
 
@@ -13,8 +16,21 @@ app.listen(PORT, () => {
 });
 
 app.get("/healthz", handlerReadiness);
+app.get("/metrics", handlerRequestCount);
+app.get("/reset", handlerResetRequestCount)
 
 async function handlerReadiness(req: Request, res: Response) : Promise<void> {
+    res.set("Content-Type", "text/plain; charset=utf-8");
+    res.send("OK");
+}
+
+async function handlerRequestCount(req: Request, res: Response) : Promise<void> {
+    res.set("Content-Type", "text/plain; charset=utf-8");
+    res.send(`Hits: ${chirpyStateData.fileserverHits}`);
+}
+
+async function handlerResetRequestCount(req: Request, res: Response) : Promise<void> {
+    chirpyStateData.fileserverHits = 0;
     res.set("Content-Type", "text/plain; charset=utf-8");
     res.send("OK");
 }
@@ -30,4 +46,10 @@ function middlewareLogResponses(req: Request, res: Response, next: NextFunction)
     });
 
     next();
+}
+
+function middlewareMetricsInc(req: Request, res: Response, next: NextFunction) {
+  chirpyStateData.fileserverHits++;
+
+  next();
 }

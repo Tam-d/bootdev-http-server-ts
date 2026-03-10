225910c3e17a7ee5019575bb6c22e7787edf3cbe
Tam <5542463+Tam-d@users.noreply.github.com>
2026-03-10 15:10:07 +0000

    fix whitespace

--- src/handle_chirps.ts
+++ src/handle_chirps.ts
@@ -8,12 +8,12 @@ export async function validateChirp(req: Request, res: Response) : Promise<void>
             throw Error("Chirp is too long");
         }
         res.set("Content-Type", "application/json");
-        res.send(JSON.stringify({"valid": true}));
-            
-        } catch (error) {
-            res.set("Content-Type", "application/json");
-            res.status(400).send(
-                JSON.stringify({"error": (error as Error).message})
-            );
-        }
+        res.send(JSON.stringify({"valid": true}));   
+    }
+    catch (error) {
+        res.set("Content-Type", "application/json");
+        res.status(400).send(
+            JSON.stringify({"error": (error as Error).message})
+        );
+    }
 }

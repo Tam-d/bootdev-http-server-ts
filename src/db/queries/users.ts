c07cf43022ada367b0c50b74b6f213bc7a6317ec
Tam <5542463+Tam-d@users.noreply.github.com>
2026-03-31 00:35:07 +0000

    Add query to update user to ChirpyRed

--- src/db/queries/users.ts
+++ src/db/queries/users.ts
@@ -21,6 +21,16 @@ export async function updateUser(user: User) {
   return result;
 }
 
+export async function upgradeUserToRed(userId: string) {
+  const [result] = await db
+    .update(users)
+    .set({isChirpyRed: true})
+    .where(eq(users.id, userId))
+    .returning();
+
+  return result;
+}
+
 export async function getUser(email: string) {
   const [result] = await db.select()
     .from(users)
